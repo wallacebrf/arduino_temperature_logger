308cc0712894b7e96e47b5f531f56a432dc503d5
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2025-04-14 16:57:38 -0500

    Update equipment_cabinet_add.php

--- equipment_cabinet_add.php
+++ equipment_cabinet_add.php
@@ -5,25 +5,19 @@
 //USER VARIABLES
 //***************************************
 $measurement="House_Temp_Hum";
-$email_file_name="equipment_cabinet_mail.php";
 $config_file_location="/volume1/web/config/config_files/config_files_local/house_config.txt";
-
-
 //***************************************
 //START OF CODE
 //***************************************
-
-error_reporting(E_ALL ^ E_NOTICE);
+error_reporting(E_NOTICE);
 $data = file_get_contents("".$config_file_location."");
 $pieces = explode(",", $data);
 $influxdb_host=$pieces[12];
 $influxdb_port=$pieces[13];
 $influxdb_name=$pieces[14];
-$influxdb_user=$pieces[15];
 $influxdb_pass=$pieces[16];
 $script_enable=$pieces[17];
-$sensor_name=$pieces[18];
-$max_temp=$pieces[19];
+$sensor_name="Garage_Outside";
 include $_SERVER['DOCUMENT_ROOT']."/functions.php";
 require $_SERVER['DOCUMENT_ROOT']."/admin/vendor/autoload.php";
 
@@ -35,22 +29,16 @@ $generic_error="";
 if ($script_enable==1){
 	
 	
-	[$middle_temp_whole, $generic_error] = test_input_processing($_GET['middle_temp_whole'], 0, "numeric", 0, 150);
-	[$middle_temp_fract, $generic_error] = test_input_processing($_GET['middle_temp_fract'], 0, "numeric", 0, 100);
-	[$middle_temp_status, $generic_error] = test_input_processing($_GET['middle_temp_status'], 0, "numeric", 0, 1);
-	[$cold_side_temp_whole, $generic_error] = test_input_processing($_GET['cold_side_temp_whole'], 0, "numeric", 0, 150);
-	[$cold_side_temp_fract, $generic_error] = test_input_processing($_GET['cold_side_temp_fract'], 0, "numeric", 0, 100);
-	[$cold_side_temp_status, $generic_error] = test_input_processing($_GET['cold_side_temp_status'], 0, "numeric", 0, 1);
-	[$cold_badsensorcount, $generic_error] = test_input_processing($_GET['cold_badsensorcount'], 0, "numeric", 0, 4294967296);
-	[$middle_badsensorcount, $generic_error] = test_input_processing($_GET['middle_badsensorcount'], 0, "numeric", 0, 4294967296);
-	
-	$sensor1_influx=$middle_temp_whole+($middle_temp_fract/100);
-	$sensor2_influx=$cold_side_temp_whole+($cold_side_temp_fract/100);
-	$sensor1_sensor2_average_influx=($sensor1_influx+$sensor2_influx)/2;
-	
-	$post_url="".$measurement.",sensor_name=$sensor_name sensor1=$sensor1_influx,sensor2=$sensor2_influx,average=$sensor1_sensor2_average_influx";
+	if ($_GET['middle_sensor']==4096.0 || $_GET['left_sensor']==4096.0 || $_GET['right_sensor']==4096.0){
+		include $_SERVER['DOCUMENT_ROOT']."/admin/garage_mail.php";
+	}
 
-
+	[$sensor1_influx, $generic_error] = test_input_processing($_GET['middle_sensor'], 0.0, "float", -100.0, 120.0);
+	[$sensor2_influx, $generic_error] = test_input_processing($_GET['left_sensor'], 0.0, "float", -100.0, 120.0);
+	[$sensor3_influx, $generic_error] = test_input_processing($_GET['right_sensor'], 0.0, "float", -100.0, 120.0);
+	$sensor_average_influx=($sensor1_influx+$sensor2_influx+$sensor3_influx)/3.0;
+	
+	$post_url="".$measurement.",sensor_name=$sensor_name sensor1=$sensor1_influx,sensor2=$sensor2_influx,sensor3=$sensor3_influx,average=$sensor_average_influx";
 
 $client = new InfluxDB2\Client(["url" => "http://".$influxdb_host.":".$influxdb_port."", "token" => "".$influxdb_pass."",
     "bucket" => "".$influxdb_name."",
@@ -62,13 +50,6 @@ $write_api->write($post_url);
 $write_api->close();
 $client->close();
 
-
-	print $sensor1_sensor2_average_influx;
-	if ($sensor1_sensor2_average_influx >$max_temp){
-		include_once ''.$email_file_name.'';
-		print "over temp";
-	}else{
-		print "under temp";
-	}
+	print $sensor_average_influx;
 }
 ?>
